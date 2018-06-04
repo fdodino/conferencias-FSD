1ca4755465d3694a98e4bec53821df46395b84f5
Fernando Dodino <fernando.dodino@gmail.com>
2018-06-04 12:24:21 -0300

    Simplifying unused variables

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,26 +1,23 @@
-import { version } from '../../package.json'
-import { Router } from 'express'
+import {version} from '../../package.json'
+import {Router} from 'express'
 import TalkService from "../services/talkService"
 
 const talkService = new TalkService()
 
-export default ({ config, db }) => {
+export default({config, db}) => {
 	let api = Router()
 
 	api.get('/talks', (req, res) => {
-		const searchValue = req.params.searchValue || ""
 		res.json(talkService.findAll())
-	}
-)
+	})
 	api.get('/talks/:searchValue', (req, res) => {
-			const searchValue = req.params.searchValue || ""
-			res.json(talkService.filter(searchValue))
-		}
-	)
+		const searchValue = req.params.searchValue || ""
+		res.json(talkService.filter(searchValue))
+	})
 
 	// perhaps expose some API metadata at the root
 	api.get('/', (req, res) => {
-		res.json({ version })
+		res.json({version})
 	})
 
 	return api
