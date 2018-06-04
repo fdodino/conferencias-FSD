71b0abe941eefdcec92db231a2fae3510dc4ed48
Fernando Dodino <fernando.dodino@gmail.com>
2018-06-04 10:51:43 -0300

    Simplifying listView.js

--- src/components/listView.js
+++ src/components/listView.js
@@ -3,11 +3,6 @@ import React, {
 } from 'react'
 
 export default class ListView extends Component {
-    constructor(props) {
-        super(props)
-        this.items = this.props.items
-    }
-
     render() {
         return (
             this.props.items.map(talk =>
